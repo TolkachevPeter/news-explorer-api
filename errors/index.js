4702a4c1c42d28d2b8357aebee7cd71c892f1a9d
TolkachevPeter <peter.tolkachev@gmail.com>
2020-11-15 19:13:00 +0300

    add index in errors

---
+++ errors/index.js
@@ -0,0 +1,11 @@
+const BadRequestError = require('./bad-request-error');
+const NotFoundError = require('./not-found-error');
+const UnauthorizedError = require('./unauthorized-error');
+const ConflictError = require('./conflict');
+
+module.exports = {
+  BadRequestError,
+  NotFoundError,
+  UnauthorizedError,
+  ConflictError,
+};
