41b64760838bc30e1c9172e78cbcd94dba3214e9
TolkachevPeter <peter.tolkachev@gmail.com>
2020-11-15 20:51:36 +0300

    fix port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const { errors } = require('celebrate');
 const usersRouter = require('./routes/users.js');
 const articlesRouter = require('./routes/articles');
